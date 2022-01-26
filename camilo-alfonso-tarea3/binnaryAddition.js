469e7cd895f86d158990aa4fa6899d9a911d32a7
Camilo Andres Alfonso Suarez <c.alfonso@globant.com>
2022-01-26 13:14:21 -0500

    task 3 - katas

---
+++ camilo-alfonso-tarea3/binnaryAddition.js
@@ -0,0 +1,8 @@
+/* Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+The binary number returned should be a string.
+
+Examples:(Input1, Input2 --> Output (explanation))) */
+
+function addBinary(a,b) {
+    return (a+b).toString(2)
+}
---
+++ camilo-alfonso-tarea3/oddEven.js
@@ -0,0 +1,13 @@
+/* Given a list of integers, determine whether the sum of its elements is odd or even.
+   Give your answer as a string matching "odd" or "even".
+   If the input array is empty consider it as: [0] (array with a zero). 
+*/
+
+function oddOrEven(array) {
+   let total = array.reduce((prev, curr) => prev+ curr, 0)
+
+   if (total % 2){
+      return ('odd')
+   }else
+      return ('even')
+ }
