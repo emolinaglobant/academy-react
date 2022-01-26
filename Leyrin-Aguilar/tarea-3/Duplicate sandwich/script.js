45504570a65c7da097cb82fd36888a665de9f5c6
Leyrin Bridneys Aguilar Jorge <leyrin.aguilar@globant.com>
2022-01-26 10:27:53 -0500

    tarea 3

---
+++ Leyrin-Aguilar/tarea-3/Duplicate sandwich/script.js
@@ -0,0 +1,8 @@
+function duplicateSandwich(a) {
+  for (let i = 0; i < a.length - 1; i++)
+    if (a.indexOf(a[i]) !== a.lastIndexOf(a[i]))
+      return a.slice(a.indexOf(a[i]) + 1, a.lastIndexOf(a[i]));
+}
+console.log(
+  duplicateSandwich(["None", "Hello", "Example", "hello", "None", "Extra"])
+);
