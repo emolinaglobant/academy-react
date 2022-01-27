856e3c2e088f4e7e9c4384188b6a62016588e95a
Esteban Salas <esteban.salas@globant.com>
2022-01-27 14:28:10 -0500

    Se optimizan las funciones

--- Esteban-Salas-Posada/tarea-3/odd-or-even.js
+++ Esteban-Salas-Posada/tarea-3/odd-or-even.js
@@ -1,13 +1,7 @@
 function oddOrEven(array) {
-    //enter code here
    let sum = 0;
-   array.forEach((element, index) => {
+   array.forEach((element) => {
      sum += element;
-     console.log(sum);
    });
-   if(sum === 0 || sum%2==0){
-     return 'even';
-   }else{
-     return 'odd';
-   }
+   sum === 0 || sum%2==0 ? 'even' : 'odd';
  }
