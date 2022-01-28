783467b87b66aa769e920e04f4c9cf86417ebae1
andres felipe ibagon rodriguez <ibagonandresf@gmail.com>
2022-01-28 15:00:19 -0500

    Tarea numero 3

---
+++ andres-ibagon/tarea-3/small__enough-Beginner.js
@@ -0,0 +1,6 @@
+/*You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+
+You can assume all values in the array are numbers.*/
+
+
+const smallEnough = (a, limit) => !a.some((e) => e > limit);
