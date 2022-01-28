d1c271d1804a9cf5f4045045cb42c038cac1c970
marlyGB <97561539+marlyGB@users.noreply.github.com>
2022-01-28 11:00:29 -0500

    Marly galeano - Tareas 1 y 2 (#69)
    * Tarea 1
    
    * Tarea 2
    
    * Tareas 1 y 2
    
    * Tarea Numero 3
    
    * Tarea 4 calculadora
    
    * Tarea 2 actualizada segun PR anterior

---
+++ marly-galeano/tarea-3/smallEnough.js
@@ -0,0 +1,3 @@
+function smallEnough(a, limit) {
+  return a.every((x) => x <= limit);
+}
---
+++ marly-galeano/tarea-4/js/calculator.js
@@ -0,0 +1,21 @@
+function display(val) {
+  document.getElementById("result").value += val;
+  return val;
+}
+
+function solve() {
+  let x = document.getElementById("result").value;
+  let y = eval(x);
+  document.getElementById("result").value = y;
+  return y;
+}
+function clearScreen() {
+  document.getElementById("result").value = "";
+}
+function del() {
+  let x = document.getElementById("result").value;
+  let y = x.slice(0, -1);
+  document.getElementById("result").value = y;
+  return y;
+}
+function percentage() {}
