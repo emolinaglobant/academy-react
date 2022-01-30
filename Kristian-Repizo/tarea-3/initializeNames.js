c62ecae7cc089e16991f2b0f1719430af6281892
Kristian Camilo Repizo Castro <kristian.repizo@globant.com>
2022-01-30 16:57:54 -0500

    tarea 3 y 4

--- Kristian-Repizo/tarea-3/initializeNames.js
+++ Kristian-Repizo/tarea-3/initializeNames.js
@@ -1,11 +1,3 @@
-function initializeNames(name){
-    let array = name.split(' ');
-    for(let i in array){
-        if(i > 0 && i < array.length - 1){
-            array[i] = array[i].slice(0, 1) + '.';
-        }
-    }
-    return array.join(' ');
-}
+const initializeNames = name => name.replace(/ (\w)\w*(?= )/g, ' $1.');
 
 console.log(initializeNames('Alice Betty Catherine Davis'));
