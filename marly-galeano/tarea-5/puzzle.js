c1729d1ee4bf9a4ebc4681455aeaa15869f3f240
marlyGB <97561539+marlyGB@users.noreply.github.com>
2022-02-02 09:52:17 -0500

    Marly galeano - Slide Puzzle (#112)
    * Tarea 1
    
    * Tarea 2
    
    * Tareas 1 y 2
    
    * Tarea 2 actualizada segun PR anterior
    
    * Tarea 1 actualizada segun comentarios del PR anterior
    
    * Tarea 1 nueva actualizacion
    
    * Tarea 5 - slide puzzle
    
    * Tarea 5 actualizada - Puzzle con SOLID
    
    * Tarea 5 - puzzle - actualizada, funciones ordenadas alfabeticamente

---
+++ marly-galeano/tarea-5/puzzle.js
@@ -0,0 +1,59 @@
+function exchangeClass(elementOne, elementTwo) {
+  var class1 = getElementClass(elementOne);
+  var class2 = getElementClass(elementTwo);
+  elementOne.className = class2;
+  elementTwo.className = class1;
+}
+
+function exchangeId(tile, emptySpace) {
+  tile.id = emptySpace.id;
+  emptySpace.removeAttribute("id");
+}
+
+function exchangeValue(elementOne, elementTwo) {
+  var value1 = getElementValue(elementOne);
+  var value2 = getElementValue(elementTwo);
+  elementOne.value = value2;
+  elementTwo.value = value1;
+}
+
+const getElementClass = (element) => element.classList[0];
+
+const getElementValue = (element) => element.value;
+
+const getEmptySpaceById = () => document.getElementById("emptySpace");
+
+function getPosition(element) {
+  var position = element.getAttribute("data-position");
+  position = [...position];
+  return [parseInt(position[0]), parseInt(position[1])];
+}
+
+function neighborhoodCheck(tile) {
+  var tilePosition = getPosition(tile);
+  var emptySpace = getEmptySpaceById();
+  var emptySpacePosition = getPosition(emptySpace);
+
+  if (
+    tilePosition[0] == emptySpacePosition[0] ||
+    tilePosition[1] == emptySpacePosition[1]
+  ) {
+    //same column or row
+
+    if (
+      emptySpacePosition[0] == tilePosition[0] + 1 ||
+      tilePosition[0] == emptySpacePosition[0] + 1 ||
+      emptySpacePosition[1] == tilePosition[1] + 1 ||
+      tilePosition[1] == emptySpacePosition[1] + 1
+    ) {
+      // consecutive rows or columns
+      slide(tile, emptySpace);
+    }
+  }
+}
+
+function slide(tile, emptySpace) {
+  exchangeValue(tile, emptySpace);
+  exchangeClass(tile, emptySpace);
+  exchangeId(tile, emptySpace);
+}
