a3221524661bb92c1d24dcc69f079f6c0d439089
Yefer Lesvier Garcia Fernandez <yefer.garcia@globant.com>
2022-02-02 17:16:45 -0500

    Actualizacion De Tarea3

--- Yefer-garcia/tarea3-globant/sandichw.js
+++ Yefer-garcia/tarea3-globant/sandichw.js
@@ -1,26 +1,15 @@
+// ! sandwich 1
+function duplicateSandwich(a) {
+let set = new Set(), x;
+[...a].forEach((i, ind) => (set.add(i), set.size < ind + 1 && x === undefined && (x = i)))
+return a.slice(a.indexOf(x)+1, a.lastIndexOf(x))
+}
+duplicateSandwich(["None", "Hello", "Example", "hello", "None", "Extra"])
+        
 
-// function duplicateSandwich(a) {
-//   for (let i = 0; i < a.length; i++)
-//   if (a.indexOf(a[i]) !== a.lastIndexOf(a[i]))
-//   return a.slice(a.indexOf(a[i]) + 1, a.lastIndexOf(a[i]));
-// }
-// console.log(
-//   duplicateSandwich(["None", "Hello", "Example", "hello", "None", "Extra"])
-// );
+ 
 
-// function sandwich(a) {
-
-//     a.forEach((a,i)=>a[i] !== a.lastIndexOf(a[i])&&(a.slice(a.indexOf(a[i]) + 1, a.lastIndexOf(a[i]))))
-// }
-// console.log(
-//   sandwich(["None", "Hello", "Example", "hello", "None", "Extra"])
-// );
-
-  
-
-
-
-// !sandwich
+// ! sandwich 2
 const arreglo=["None", "Hello", "Example", "hello", "None", "Extra"]
 const solicitud=[]
 let sandwich= arreglo.forEach((posicion)=>posicion=='Hello'&&(solicitud.push(posicion))||posicion=='Example' &&(solicitud.push(posicion)) ||posicion=='hello'&&(solicitud.push(posicion)))
