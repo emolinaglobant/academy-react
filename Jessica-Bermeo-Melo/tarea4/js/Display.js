d027072c73b8bc6088097f349bd2286ea3beff4d
JessicaBermeoGlobant <97971341+JessicaBermeoGlobant@users.noreply.github.com>
2022-02-03 11:59:43 -0500

    Jessica bermeo melo (#91)
    * Tarea 1
    
    * tarea 2 y 3 commit
    
    * tarea 4 commit

---
+++ Jessica-Bermeo-Melo/tarea4/js/Display.js
@@ -0,0 +1,51 @@
+class Display {
+    constructor(displayPreviousValue, displayCurrentValue){
+        this.displayPreviousValue = displayPreviousValue;
+        this.displayCurrentValue  = displayCurrentValue;
+        this.calculator           = new Calculator();
+        this.typeOperation        = undefined;
+        this.previousValue        = '';
+        this.currentValue         = '';
+        this.signs                = { division:'%', multiplication:'*',subtraction:'-',sum:'+',}
+    }
+
+    addNumber(number){
+        if (number === '.' && this.currentValue.includes('.')) return
+        this.currentValue = this.currentValue.toString() + number.toString();
+        this.printValues();
+    }
+
+    calculate(){
+        const previousValue = parseFloat(this.previousValue);
+        const currentValue = parseFloat(this.currentValue);
+
+        if(isNaN(previousValue) ||isNaN( currentValue))return
+        this.currentValue = this.calculator[this.typeOperation](previousValue,currentValue);
+    }
+
+    compute(type){
+        this.typeOperation !== 'equals' && this.calculate();
+        this.typeOperation = type;
+        this.previousValue = this.currentValue || this.previousValue;
+        this.currentValue = '';
+        this.printValues();
+    }
+    delete(){
+        this.currentValue = this.currentValue.toString().slice(0,-1);
+        this.printValues();
+    }
+
+    deleteAll(){
+        this.currentValue = '';
+        this.previousValue = '';
+        this.typeOperation = undefined;
+        this.printValues();
+    }
+
+    printValues(){
+        this.displayCurrentValue.textContent = this.currentValue;
+        this.displayPreviousValue.textContent = `${this.previousValue} ${this.signs[this.typeOperation] || ''}`;
+    }
+
+}
+
