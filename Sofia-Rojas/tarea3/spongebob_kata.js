d90fc6806a13f26f47a672ac396d2a4fe10adc8b
sofia-rojas <97647021+sofia-rojas@users.noreply.github.com>
2022-02-07 20:53:15 -0500

    Update spongebob_kata.js

--- Sofia-Rojas/tarea3/spongebob_kata.js
+++ Sofia-Rojas/tarea3/spongebob_kata.js
@@ -1,14 +1,5 @@
 function spongeMeme(sentence) {
-  let meme = "";
-  let i = 0;
-while (i < sentence.length) {
-    if(i%2===0){
-        meme+=sentence[i].toUpperCase();
-    }else{
-        meme+=sentence[i].toLowerCase();
-    } 
- i++;
-}
-return meme;
-
+    return sentence.split("").map((element, index) => {
+        return (index %2 === 0) ?  element.toUpperCase() :  element.toLowerCase(); 
+    }).join("");
 }
