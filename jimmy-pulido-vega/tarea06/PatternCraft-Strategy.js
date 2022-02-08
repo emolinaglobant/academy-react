1d4a37959009117741d1f41405643d6237174e47
Jimmy Alejandro Pulido Vega <jimmy.pulido@globant.com>
2022-02-08 15:22:06 -0500

    Entrega tarea06 PatternCreaft Strategy

---
+++ jimmy-pulido-vega/tarea06/PatternCraft-Strategy.js
@@ -0,0 +1,22 @@
+class Fly {
+    move(unit) {
+      unit.position += 10;
+    }
+  }
+  
+  class Walk {
+    move(unit) {
+      unit.position++
+    }
+  }
+  
+  class  Viking {
+    constructor() {
+      this.position = 0;
+      this.moveBehavior = new Walk();
+    }
+    
+    move() {
+      this.moveBehavior.move(this)
+    }
+  }
