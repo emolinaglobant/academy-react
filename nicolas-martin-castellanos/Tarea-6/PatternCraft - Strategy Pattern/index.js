ca7f6a58645d7ce7a2a446779e9d57d957b240d5
Nicolas Martin Castellanos <97647983+NicolasMartin23@users.noreply.github.com>
2022-02-08 19:10:13 -0500

    Nicolas martin castellanos (#145)
    * Tarea 1
    
    * send magazine to pr
    
    * add grid and flexbox layouts
    
    * Envio Tarea1 corregida y Tarea de Form para PR
    
    * send uploading magazine to PR
    
    * sending new upload magazine to PR
    
    * new PR
    
    * Envio tarea 1 corregida tarea 3 realizada y tarea 4 realizada
    
    * send to PR tarea#5
    
    * send to PR tarea5
    
    * send tarea#6 to PR

---
+++ nicolas-martin-castellanos/Tarea-6/PatternCraft - Strategy Pattern/index.js
@@ -0,0 +1,21 @@
+class Fly {
+    move(unit) {
+      unit.position += 10;
+    }
+  }
+  
+  class Walk {
+    move(unit) {
+      unit.position += 1;
+    }
+  }
+  
+  class Viking {
+    constructor() {
+      this.position = 0;
+      this.moveBehavior = new Walk();
+    }
+    move() {
+      this.moveBehavior.move(this);
+    }
+  } 
