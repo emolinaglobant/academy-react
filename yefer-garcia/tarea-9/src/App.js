5e31855cd028a201efc22973a79858d3e93aef57
Yefer Lesvier Garcia Fernandez <yefer.garcia@globant.com>
2022-02-10 15:18:45 -0500

    tarea-9

---
+++ yefer-garcia/tarea-9/src/App.js
@@ -0,0 +1,28 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <body>
+    <article class="revista">
+      <header class="letter-1">
+        <section class="letter-1-1">
+          <h1>GLAMOUR</h1>
+        </section>
+        <section class="letter-1-2">
+          <h3>Shakira!</h3>
+        </section>
+      </header>
+      <footer class="letter-2">
+        <p class="text">THE</p>
+        <h2 class="numero">100</h2>
+        <h2 class="new">BEST NEW</h2>
+        <h2 class="ideas">HAIR IDEAS</h2>
+        <p>EVERY TEXTURE,EVERY LENGTH,EVERY STYLE-THIS IS YOU PLAYBOOK</p>
+      </footer>
+    </article>
+  </body>
+  );
+}
+
+export default App;
