915a4e9388d4b7784595ec40b6f2f521a7fc0832
luishurtado-globant <97626965+luishurtado-globant@users.noreply.github.com>
2022-02-14 15:55:40 -0500

    React Magazine (#202)
    * React Magazine
    
    * cover image added

---
+++ luis-hurtado/tarea-9/react-magazine/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <main class="container">
+            <h2 id="title">VOGUE</h2>
+            <h3 id="subtitle">The rebirth of style</h3>    
+            <article class="description">
+                <section>
+                    <p>Model, mentor, provocateur, icon</p>
+                    <p>Arya takes us to Paris and back</p> 
+                </section>
+            </article>
+            <article class="quotes">
+                <section>
+                    <p>"Fearless, fun, and defying Hollywood expectations"</p>
+                    <p>"21 Inspirational Australian Women of 2022"</p>
+                </section>
+            </article>
+        </main>
+    </header>
+    </div>
+  );
+}
+
+export default App;
