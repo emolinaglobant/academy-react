ac26b3b25cbcf15fe3a1cb295860d4187a203573
Yefer Lesvier Garcia Fernandez <yefer.garcia@globant.com>
2022-02-15 10:36:03 -0500

    actu-tarea-7-merge

--- yefer-garcia/tarea-7-globant/testing.test.js
+++ yefer-garcia/tarea-7-globant/testing.test.js
@@ -1,56 +1,19 @@
-const {
-    getName,
-    getEmail,
-    getNumberPassword,
-    verifyInput,
-    makePassword,
-  } = require("./main");
-  
-  test("Ingrese su nombre", () => {
-    expect(getName({ target: { value: "Yeferdon" } })).toBe("Yeferdon");
+const { verifyPassword, verifyUser } = require("./index");
+
+describe("logIn test", () => {
+  test("si la contraseña tiene mayúscula y 8 caracteres, true", () => {
+    expect(verifyPassword("Password123")).toBe(true);
   });
-  
-  test("Ingrese su Email", () => {
-    expect(getEmail({ target: { value: "koyaher21@gmail.com" } })).toBe(
-      "koyaher21@gmail.com"
-    );
+
+  test("Si el usuario tiene un mayuscula, true", () => {
+    expect(verifyUser("Yeferdon")).toBe(true);
   });
-  
-  test("Comprueba que el correo incluya @", () => {
-    expect(getEmail({ target: { value: "koyaher21@gmail.com" } })).toMatch(
-      /@/
-    );
+
+  test("si la contraseña no tiene mayúsculas y 8 caracteres, false", () => {
+    expect(verifyPassword("51028")).toBe(false);
   });
-  
-  test("Ingrese su password", () => {
-    expect(getNumberPassword({ target: { value: 9811902 } })).toBe(9811902);
+
+  test("si el usuario no tiene un mayuscula,false", () => {
+    expect(verifyUser("yeferdon")).toBe(false);
   });
-  
-  test("El password es invalido", () => {
-    expect(getNumberPassword({ target: { value: "Yeferdon" } })).toBeNaN();
-  });
-  
-  test("Verify that array contains certain values", () => {
-    let array = ["Yeferdon", "koyaher21@gmail.com", 9811902];
-    expect(makePassword(...array)).toContain(9811902);
-  });
-  
-  test("Alerta de bienvenida", () => {
-    const alert = { message: "Bienvenido" };
-    const getMeessage = jest.fn((alert) => alert.message);
-  
-    getMeessage(alert);
-    expect(getMeessage).toHaveReturnedWith("Bienvenido");
-  });
-  
-  test("La función VerifyInput retorna true", () => {
-    const inputEmpty = jest.fn((verifyInput) => true);
-    inputEmpty();
-    expect(inputEmpty).toHaveReturned();
-  });
-  
-  test("Se presiona el botón Formulario", () => {
-    const btnPay = jest.fn();
-    btnPay();
-    expect(btnPay).toHaveBeenCalled();
-  });
+});
