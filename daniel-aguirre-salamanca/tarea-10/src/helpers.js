e8eea861d36a6dacad3e8512e8d4b09f3ebe39a0
Daniel Aguirre Salamanca <d.aguirre@globant.com>
2022-02-16 17:20:21 -0500

    small change in helpers

--- daniel-aguirre-salamanca/tarea-10/src/helpers.js
+++ daniel-aguirre-salamanca/tarea-10/src/helpers.js
@@ -1,20 +1,20 @@
 import { ReactComponent as Heart } from "./assets/heart.svg";
 
-const getRandomMovie = (movies) => {
-  const moviesNames = Object.keys(movies);
-  const randomMovie = moviesNames[(moviesNames.length * Math.random()) << 0];
-  return { name: randomMovie, emoji: movies[randomMovie] };
-}
-
+const converToHearts = (lifes) => {
+  const emptyArray = Array.apply(null, Array(lifes));
+  return emptyArray.map((emptyElement, index) => (
+    <Heart key={index} width={75} height={75} />
+  ));
+};
 const dropMovie = (movie, moviesState) => {
   const movies = { ...moviesState };
   delete movies[movie.name];
   return movies;
-}
-
-const converToHearts = (lifes) => {
-  const emptyArray = Array.apply(null, Array(lifes));
-  return emptyArray.map((emptyElement, index) => <Heart key={index} width={75} height={75}/>);
-}
+};
+const getRandomMovie = (movies) => {
+  const moviesNames = Object.keys(movies);
+  const randomMovie = moviesNames[(moviesNames.length * Math.random()) << 0];
+  return { name: randomMovie, emoji: movies[randomMovie] };
+};
 
 export { getRandomMovie, dropMovie, converToHearts };
