d963a1dd17300850c9e3f0e4cec5fc61a0074ebc
Juan Aristizabal <juan.aristizabal@globant.com>
2022-02-16 22:12:20 -0500

    tarea10 guess game

---
+++ juan-aristizabal-bedoya/tarea10-guess-game/guess-game/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Game from './components/game';
+
+function App() {
+  return (
+    <article className="App">
+     <Game/>
+    </article>
+  );
+}
+
+export default App;
