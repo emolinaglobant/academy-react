8e3473b6cedbfb3f0797a2e13d5f27ba0819545c
Marvelys <97606164+MarvelysTorres@users.noreply.github.com>
2022-02-18 09:37:09 -0500

    Tarea 10 (#229)

---
+++ Marvelys-Torres/tarea-10/guess-the-movie/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Game } from "./views/Game.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <Game />
+    </div>
+  );
+}
+
+export default App;
---
+++ Marvelys-Torres/tarea-10/guess-the-movie/src/data/movies.js
@@ -0,0 +1,32 @@
+export const movies = [
+  {
+    id: 1,
+    emojis: ["🤖", "🤺", "👋", "👶"],
+    title: "terminator",
+  },
+  {
+    id: 2,
+    emojis: ["⚡", "🧑", "🌠", "🛕"],
+    title: "harry potter",
+  },
+  {
+    id: 3,
+    emojis: ["🚢", "💃", "🕺", "🌊"],
+    title: "titanic",
+  },
+  {
+    id: 4,
+    emojis: ["👩‍🦰", "🔪", "👊", "🟨"],
+    title: "kill bill",
+  },
+  {
+    id: 5,
+    emojis: ["💊", "🔴", "🔵", "💻"],
+    title: "the matrix",
+  },
+  {
+    id: 6,
+    emojis: ["🎭", "👽", "🐶", "💑"],
+    title: "the mask",
+  },
+];
