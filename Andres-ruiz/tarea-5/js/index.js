53d74ad95fda60e5a1b5e7389dccd142cc9e277d
andresCruiz <89539866+andresCruiz@users.noreply.github.com>
2022-02-18 09:08:22 -0500

    Andres ruiz tarea 8 (#213)
    * tarea #3
    
    * validacion formularios javascript
    
    * crear proyectos react

---
+++ Andres-ruiz/tarea-5/js/index.js
@@ -0,0 +1,13 @@
+const slider = {primero:[1, 2, 3, 4], segundo:[5, 6, 7, 8], tercero:[9, 10, 11,12 ], cuarto:[13, 14, 15, null]
+}
+
+function recorrer(n) {
+    for(let i=0; i<= n.length; i++){
+        const puzzel = document.getElementById('puzzel');
+        const numero = document.createElement('button');
+        puzzel.appendChild(numero);
+     }
+}
+recorrer(slider.primero);
+
+
