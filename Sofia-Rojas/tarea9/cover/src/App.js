c64befc87cf2540d7b953032f6aaf7b54c6abe9d
Sofia Rojas Lopez <sofia.rojas@globant.com>
2022-02-21 10:51:01 -0500

    tarea 9

---
+++ Sofia-Rojas/tarea9/cover/src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        
+      </header>
+
+      
+      <body>
+        <article class="bg"  >
+        <section class="flex-container">
+            
+        
+           
+            <h1 class="header">VOGUE</h1>
+            <h2 class="text-1">Invitée d'honnoeur</h2>
+            <h2 class="text-2">Victoria</h2>
+            <h2 class="text-3">Beckham</h2>
+            <h3 class="Paragraph">SES <span class="bold">PASSIONS</span>, SON DESTIN DE <span class="bold">MODE</span>, SA VIE <span class="bold">PRIVÉE</span>, SES <span class="bold">SECRETS</span> DE BEAUTÉ</h3>
+            <h3 class="Paragraph-2"> avec David Beckham, Diana Ross, Marc Jacobs...</h3>    
+                
+       
+                
+        </section>
+        </article>
+    </body>
+        
+    </div>
+  );
+}
+
+export default App;
