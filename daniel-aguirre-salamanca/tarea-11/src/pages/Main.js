c8c8602294eb27e0c895e4c9c044a82e8c9e55e4
daguirreglobant <97563902+daguirreglobant@users.noreply.github.com>
2022-02-21 08:41:37 -0500

    tarea 11 (#243)

---
+++ daniel-aguirre-salamanca/tarea-11/src/pages/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TodoList from "../components/TodoList";
+import Card from "../components/UI/Card";
+const Main = ({ data, onDelete }) => {
+  return (
+    <Card className="card--in-column">
+      <h1>Welcome to your Todos!</h1>
+      <TodoList data = {data} onDelete={onDelete}/>
+    </Card>
+  );
+};
+
+export default Main;
