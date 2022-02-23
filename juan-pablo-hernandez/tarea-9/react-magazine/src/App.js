2ac99c8b01fa75ea23e6a9ab46abef4c2b43371a
juan-hm <97625519+juan-hm@users.noreply.github.com>
2022-02-22 20:08:39 -0500

    Tarea 9 (#251)

---
+++ juan-pablo-hernandez/tarea-9/react-magazine/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <main className="magazine-container">
+        <article className="magazine-cover">
+            <section className="main-info">
+                <h1>NATIONAL<br/>GEOGRAPHIC</h1>
+                <h2>WE ARE NOT ALONE</h2>
+                <p>Scientists say there must be other life in the universe.<br/>Here's how they're searching for it.</p>
+            </section>
+        </article>
+    </main>
+  );
+}
+
+export default App;
