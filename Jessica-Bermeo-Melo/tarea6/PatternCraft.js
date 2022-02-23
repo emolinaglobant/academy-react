bb1d678d86485a1b447a45c01cf8aea3aff26816
Jessica Alejandra Bermeo Melo <jessica.bermeo@globant.com>
2022-02-23 11:02:29 -0500

    tareas 6,7,8 y ajustes del orden del css de tareas anteriores

---
+++ Jessica-Bermeo-Melo/tarea6/PatternCraft.js
@@ -0,0 +1,24 @@
+class Fly {
+    move(unit) {
+      unit.position += 10;
+    }
+  }
+  
+  class Walk {
+    move(unit) {
+        unit.position++
+    }
+  }
+  
+  class Viking {
+    constructor() {
+      this.position     = 0;
+      this.moveBehavior = new Walk();
+    }
+  
+    move() {
+      this.moveBehavior.move(this);
+    }
+  }
+
+  
