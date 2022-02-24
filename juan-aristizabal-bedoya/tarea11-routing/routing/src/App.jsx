b198dfb4b93fd900e0509b71ef39e6ecec8a12e4
Juan Aristizabal <juan.aristizabal@globant.com>
2022-02-24 15:44:14 -0500

    ToDo List fixed errors

--- juan-aristizabal-bedoya/tarea11-routing/routing/src/App.jsx
+++ juan-aristizabal-bedoya/tarea11-routing/routing/src/App.jsx
@@ -1,28 +1,83 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route, Router, Link } from "react-router-dom";
-import Lunes from "./pages/Lunes";
-import Martes from "./pages/Martes";
-import Miercoles from "./pages/Miercoles";
-import Jueves from "./pages/Jueves";
-import Viernes from "./pages/Viernes";
-import Sabado from "./pages/Sabado";
-import Domingo from "./pages/Domingo";
+import { Routes, Route, Link } from "react-router-dom";
+import Form from "./components/Form";
+import NewToDo from "./components/NewToDo";
+import { useState } from "react";
 
-import Navbar from "./components/Navbar";
+let data = [
+  {
+    id: "1",
+    name: "Learn JSX",
+  },
+  {
+    id: "2",
+    name: "Learn Routing",
+  },
+  {
+    id: "3",
+    name: "Play Red dead redemption",
+  },
+];
+
 function App() {
+  const [newTask, setNewTask] = useState("");
+  const [newData, setNewData] = useState(data);
+
+  const createNewTask = (e) => {
+    e.preventDefault();
+
+    if (newTask !== "") {
+      let newId = `${newData.length + 1}`;
+      let list = [...newData];
+
+      let newToDo = {
+        id: newId.toString(),
+        name: newTask,
+      };
+      list.push(newToDo);
+      setNewData(list);
+      setNewTask("");
+    }
+  };
+
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Lunes />} />
-        <Route path="/daytwo" element={<Martes />} />
-        <Route path="/daythree" element={<Miercoles />} />
-        <Route path="/dayfour" element={<Jueves />} />
-        <Route path="/dayfive" element={<Viernes />} />
-        <Route path="/daysix" element={<Sabado />} />
-        <Route path="/dayseven" element={<Domingo />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <article className="app-container">
+        <main className="viewTask-container">
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <Form
+                  newTask={newTask}
+                  setNewTask={setNewTask}
+                  createNewTask={createNewTask}
+                />
+              }
+            />
+            {newData.map((i) => {
+              return (
+                <Route
+                  path={`/${i.id}`}
+                  element={<NewToDo {...i} />}
+                  key={i.id}
+                />
+              );
+            })}
+          </Routes>
+        </main>
+        <nav className="task-nav">
+          <Link to="/">Create task</Link>
+          {newData.map((i) => {
+            return (
+              <Link to={`/${i.id}`} key={i.id}>
+                {i.name}
+              </Link>
+            );
+          })}
+        </nav>
+      </article>
+    </div>
   );
 }
 
