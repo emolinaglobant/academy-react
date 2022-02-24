bdb03235108621697259376c752615752ab2420e
Nicolas Martin Castellanos <97647983+NicolasMartin23@users.noreply.github.com>
2022-02-23 20:08:41 -0500

    tarea 9 (#237)

---
+++ nicolas-martin-castellanos/Tarea-9/src/App.js
@@ -0,0 +1,62 @@
+import './App.css';
+
+function App() {
+  return (
+    <body>
+    <div>
+    <section class="Fondo">
+               <img src="./imagenes/fondo.png"  alt=""/>
+       </section>
+       <section class="Fondo1">
+               <img src="./imagenes/fondo.png"  alt=""/>
+       </section>
+    <header class="title">
+    <h1> JORDAN</h1> 
+    </header>
+    <article>
+          <section class="box">
+          <section class="jordan">
+                      <section class="text">
+                          <h2>THE</h2>
+                      </section>
+                      <section class="text2">
+                            <h2>GREATEST</h2>
+                        </section>
+                        <section class="text3">
+                            <h2>OFF</h2>
+                        </section>
+                        <section class="text4">
+                            <h2>ALL TIME</h2>
+                        </section>
+                        <section class="subtitle">
+                            <h3>HEART OF CHAMPION</h3>
+                        </section>
+                        <section class="subtitle2">
+                            <h3>PRIVATE MOMENTS</h3>
+                        </section>
+                        <section class="subtitle3">
+                            <h3>CAREER HIGHLIGHTS</h3>
+                        </section>
+                        <section class="inf1">
+                            <h4>ESPECIAL ISSUE</h4>
+                        </section>
+                        <section class="circulo">
+                            <h4>PLUS</h4>
+                        </section>
+                        <img src="./imagenes/michael-jordan.png" alt="" />
+          </section>        
+                     <section class="codigo">
+                        <img src="./imagenes/codigo-de-barras.png" alt=""/>
+                     </section>
+          </section>   
+    </article>
+          
+              <footer>
+                <h4>CENTENNIAL ICONS &copy;</h4>
+              </footer>
+    </div>
+  </body>
+  );
+}
+
+export default App;
