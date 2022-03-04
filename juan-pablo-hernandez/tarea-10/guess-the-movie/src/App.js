017d43c325cd4432770f90d0931481d99b5092cf
Juan Pablo Hernandez Mejia <juan.hm@globant.com>
2022-03-04 12:46:51 -0500

    Tarea 10

---
+++ juan-pablo-hernandez/tarea-10/guess-the-movie/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import GameForm from './components/GameForm';
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <GameForm />
+      </main>
+    </div>
+  );
+}
+
+export default App;
