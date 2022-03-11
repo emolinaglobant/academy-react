96c399a3c91f481495c79a5c3915b086e1cf2f34
Luis Hurtado <luis.hurtado@globant.com>
2022-03-10 20:32:36 -0500

    Corección de tarea 12

--- luis-hurtado/tarea-12/routing-tasks-redux/src/components/createTask/createTask.js
+++ luis-hurtado/tarea-12/routing-tasks-redux/src/components/createTask/createTask.js
@@ -1,72 +1,65 @@
-import { useState } from "react"
-import { connect } from "react-redux";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+// import { addTasks } from "../../redux/todoSlice";
+import "../createTask/createTask.css";
 import { addTasks } from "../../redux/todoSlice";
-import "../createTask/createTask.css"
 
-const mapStateToProps = (state) => {
-    return {
-        tasks: state,
-    };
+
+const CreateTask = () => {
+  const [taskTitle, setTaskTitle] = useState("");
+  const [taskDescription, setTaskDescription] = useState("");
+
+  const dispatch = useDispatch();
+
+  const saveTask = () => {
+    if (taskTitle === "") {
+      alert("the title is empty");
+    } else {
+      setTaskTitle("");
+      setTaskDescription("");
+      dispatch(
+        addTasks({
+          title: taskTitle,
+          description: taskDescription,
+          id: Math.floor(Math.random() * 1000),
+        })
+      );
+    }
+  };
+
+  const handleTitle = (e) => {
+    setTaskTitle(e.target.value);
+  };
+  const handleDescription = (e) => {
+    setTaskDescription(e.target.value);
+  };
+
+  return (
+    <main>
+      <h1 className="mainTitle">Welcome to Taskinator</h1>
+      <article className="taskGenerator">
+        <label htmlFor="title">Title</label>
+        <input
+          name="title"
+          type="text"
+          value={taskTitle}
+          onChange={(e) => handleTitle(e)}
+          placeholder="Enter a Title"
+        />
+        <label htmlFor="description">Description</label>
+        <input
+          name="description"
+          type="text"
+          value={taskDescription}
+          onChange={(e) => handleDescription(e)}
+          placeholder="Enter a Description"
+        />
+        <button className="createButton" onClick={() => saveTask()}>
+          Add Task
+        </button>
+      </article>
+    </main>
+  );
 };
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        addTask: (obj) => dispatch(addTasks(obj)),
-    };
-};  
-
-const CreateTask = (props) => {
-
-    const [taskTitle, setTaskTitle] = useState("");
-    const [taskDescription, setTaskDescription] = useState("")
-
-
-    const add = () => {
-        if (taskTitle === "") {
-            alert("the title is empty")
-        } else {
-            props.addTask({
-                id: Math.floor(Math.random() * 100),
-                title: taskTitle,
-                description: taskDescription,
-            });
-            setTaskTitle("")
-            setTaskDescription("")
-        }
-    }
-
-    const handleTitle = (e) =>{
-        setTaskTitle(e.target.value)
-    }
-    const handleDescription = (e) => {
-        setTaskDescription(e.target.value)
-    }
-
-    return(
-        <main>
-            <h1 className="mainTitle">Welcome to Taskinator</h1>
-            <article className="taskGenerator">
-                <label htmlFor="title" >Title</label>
-                <input
-                    name="title"
-                    type="text"
-                    value={taskTitle}
-                    onChange={(e) => handleTitle(e)}
-                    placeholder="Enter a Title"
-                />    
-                <label htmlFor="description" >Description</label>
-                <input
-                    name="description"
-                    type="text"
-                    value={taskDescription}
-                    onChange={(e) => handleDescription(e)}
-                    placeholder="Enter a Description"
-                />
-                    <button className="createButton" onClick={() => add()} >
-                        Add Task
-                    </button>
-            </article>
-        </main>
-    )
-}
-export default connect(mapStateToProps, mapDispatchToProps)(CreateTask);
+export default CreateTask;
--- luis-hurtado/tarea-12/routing-tasks-redux/src/redux/todoSlice.js
+++ luis-hurtado/tarea-12/routing-tasks-redux/src/redux/todoSlice.js
@@ -1,21 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = []
+const initialState = [];
 
 const addTaskReducer = createSlice({
-    name: "tasks",
-    initialState,
-    reducers: {
-        addTasks: (state, action) => {
-            state.push(action.payload);
-            return state     
-        },
-        removeTasks: (state, action) => {
-            return state.filter((item) => item.id !== action.payload);
-        },
-    }
-})
+  name: "tasks",
+  initialState,
+  reducers: {
+    addTasks: (state, action) => {
+      state.push(action.payload);
+      return state;
+    },
+    removeTasks: (state, action) => {
+      return state.filter((item) => item.id !== action.payload);
+    },
+  },
+});
 
+export const { addTasks, removeTasks } = addTaskReducer.actions;
 
-export const {addTasks, removeTasks} = addTaskReducer.actions;
-export const reducer = addTaskReducer.reducer
+export const reducer = addTaskReducer.reducer;
